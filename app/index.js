6ee2d5408c1c362ac7f4fdfd30148e95a6791455
Oresta <orusssia@gmail.com>
2016-12-16 16:04:55 +0200

    Add MyAPP page

--- app/index.js
+++ app/index.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
   $(".toggle-sidebar").click(function(e){
     e.stopImmediatePropagation();
     $("aside").toggleClass("show");
+    $(".aside-logo").toggleClass("show-aside-logo");
     $("#overlay").toggleClass("overlay");
   });
 
   $("#overlay").click(function(e){
     e.stopImmediatePropagation();
     $("aside").toggleClass("show");
+    $(".aside-logo").toggleClass("show-aside-logo");
     $("#overlay").toggleClass("overlay");
   });
 });
